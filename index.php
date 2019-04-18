aa45cdf3710fbd3657fc6d23c1536289807a3b31
odinzero <alekseykravchenko1979@gmail.com>
2019-04-18 14:51:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
 
-echo "Hello world";
+<?php
 
-include 'web/index.php';
+// comment out the following two lines when deployed to production
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
+//require(__DIR__ . '/../vendor/autoload.php');
+//require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
+require(__DIR__ . '/vendor/autoload.php');
+require(__DIR__ . '/vendor/yiisoft/yii2/Yii.php');
+
+$config = require(__DIR__ . '/config/web.php');
+
+//echo __DIR__ ;  // C:\wamp\www\TEST_MYBLOG\web
+//die();
+
+(new yii\web\Application($config))->run();
+
